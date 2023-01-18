49995fc4dbdd95d28d0ddeee672bec70a9825410
nfottz <yblee122077@gmail.com>
2023-01-18 09:35:52 +0900

    2023-01-18

---
+++ 06_JAVA_API/src/ex06_Object/MainClass.java
@@ -0,0 +1,16 @@
+package ex06_Object;
+
+public class MainClass {
+
+	/*
+	 	java.lang.Object 클래스
+	 	1. 모든 클래스의 최상위 슈퍼 클래스이다.
+	 	2. 별도의 슈퍼 클래스를 명시하지 않는 경우 Object 클래스가 슈퍼 클래스이다.
+	 	3. 모든 것을 저장할 수 있는 만능 타입이다.
+	 */
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
