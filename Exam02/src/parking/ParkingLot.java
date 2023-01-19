859746d646e42cb65957af26e8789b639c46310f
nfottz <yblee122077@gmail.com>
2023-01-20 01:02:30 +0900

    2023-01-19 HOME

--- Exam02/src/parking/ParkingLot.java
+++ Exam02/src/parking/ParkingLot.java
@@ -33,11 +33,26 @@ public class ParkingLot {
 	}
 	
 	public void deleteCar() {
+		System.out.print("차량 번호를 입력해주세요. >>> ");
+		String carNo = sc.next();
 		
+		for(int i = 0; i < idx; i++) {		
+			 if(carNo.equals(cars[i].getCarNo())) {
+				cars[i] = cars[--idx];
+				System.out.println("차량번호 " + carNo + "차량이 삭제되었습니다.");
+				return;
+			}
+		}
+		System.out.println("해당하는 차량이 없습니다.");
 	}
 	
 	public void printAllCars() {
-		
+		System.out.println("---- " + name + " 주차 현황 ----");
+		System.out.println("현재 등록된 총 차량 : " + idx + "");
+		for(int i = 0; i < idx; i++) {
+			System.out.print("차량번호 : " + cars[i].getCarNo() 
+					+ ", 모델 : " + cars[i].getModel() + "\n");
+		}
 	}
 	
 	public void manage() {
